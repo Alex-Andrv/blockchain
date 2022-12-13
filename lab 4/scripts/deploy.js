4c0bdb51818d652603c5f5a19e2249baa5ad1f5d
alexanderandreev <sesh_1.0@niuitmo.ru>
2022-12-13 19:03:56 +0300

    ready

--- lab 4/scripts/deploy.js
+++ lab 4/scripts/deploy.js
@@ -1,19 +1,14 @@
-async function main() {
-    const [deployer] = await ethers.getSigners();
+const { deploy } = require("./lib.js");
+const {createPair} = require("./lib");
+const {ethers} = require("hardhat");
 
-    console.log("Deploying contracts with the account:", deployer.address);
 
-    console.log("Account balance:", (await deployer.getBalance()).toString());
-
-    const Token = await ethers.getContractFactory("MyToken");
-    const token = await Token.deploy();
-
-    console.log("Token address:", token.address);
-}
-
-main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-        console.error(error);
-        process.exit(1);
-    });
+(async () => {
+    const [owner] = await ethers.getSigners();
+    deploy(owner, 100_000)
+        .then(() => process.exit(0))
+        .catch((error) => {
+            console.error(error);
+            process.exit(1);
+        });
+})();
